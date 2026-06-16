60d3ee19184b07c8779f79714dc4ed6e0dd1ff10
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-16 20:41:43 -0300

    Adicionando primeira questão

---
+++ q1.js
@@ -0,0 +1,15 @@
+function contarDivisiveis() {
+
+    let contador = 0;
+
+    for(let i = 1; i <= 1000; i++){
+
+        if(i % 7 === 0){
+            contador++;
+        }
+
+    }
+
+    document.getElementById("resultado").innerHTML =
+    `Existem ${contador} números divisíveis por 7`;
+}
